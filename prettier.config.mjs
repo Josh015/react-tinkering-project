1bdacae43d8707d582e88215c61e840eb4afc8b7
josh015 <joshua.d.ols@gmail.com>
2024-06-11 12:46:21 -0400

    Removed tailwindcss from the code.

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -19,11 +19,7 @@ const config = {
   ],
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderTypeScriptVersion: '5.0.0',
-  plugins: [
-    '@ianvs/prettier-plugin-sort-imports',
-    'prettier-plugin-jsdoc',
-    'prettier-plugin-tailwindcss'
-  ]
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-jsdoc']
 };
 
 export default config;
