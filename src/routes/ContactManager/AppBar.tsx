ae4dfc0d1773256e75fc4df4de6fbdef3001096c
josh015 <joshua.d.ols@gmail.com>
2024-06-11 16:02:00 -0400

    Moved `AppBar` and related code into own component file.

---
+++ src/routes/ContactManager/AppBar.tsx
@@ -0,0 +1,133 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Menu, MenuItem } from '@mui/material';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import NewContactDialog from './NewContactDialog';
+
+const drawerWidth = 240;
+
+interface AppBarProps {
+  open: boolean;
+  toggleDrawer: () => void;
+}
+
+interface AppBarStylesProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBarStyles = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open'
+})<AppBarStylesProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}));
+
+export default function AppBar({ open, toggleDrawer }: AppBarProps) {
+  const { t } = useTranslation();
+  const prefix = 'ContactManager.Toolbar.';
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const menuOpen = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const [newContactDialogOpen, setNewContactDialogOpen] = React.useState(false);
+  const handleNewContactDialogOpen = () => {
+    setNewContactDialogOpen(true);
+    handleClose();
+  };
+  const handleNewContactDialogClose = () => {
+    setNewContactDialogOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <AppBarStyles position="absolute" open={open}>
+        <Toolbar
+          sx={{
+            pr: '24px' // keep right padding when drawer closed
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: '36px',
+              ...(open && { display: 'none' })
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            {t(`${prefix}Title`)}
+          </Typography>
+          <IconButton
+            aria-label="more"
+            aria-controls={open ? 'long-menu' : undefined}
+            aria-expanded={open ? 'true' : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={menuOpen}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left'
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left'
+            }}
+          >
+            <MenuItem onClick={handleNewContactDialogOpen}>
+              {t(`${prefix}Menu.NewContact`)}
+            </MenuItem>
+            <MenuItem disabled={true} onClick={handleClose}>
+              {t(`${prefix}Menu.ToggleTheme`)}
+            </MenuItem>
+            <MenuItem disabled={true} onClick={handleClose}>
+              {t(`${prefix}Menu.ToggleDir`)}
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBarStyles>
+
+      <NewContactDialog
+        open={newContactDialogOpen}
+        onClose={handleNewContactDialogClose}
+      />
+    </React.Fragment>
+  );
+}
--- src/routes/ContactManager/index.tsx
+++ src/routes/ContactManager/index.tsx
@@ -1,16 +1,11 @@
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import MenuIcon from '@mui/icons-material/Menu';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {
   Container,
   ListItemButton,
   ListItemIcon,
-  ListItemText,
-  Menu,
-  MenuItem
+  ListItemText
 } from '@mui/material';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
@@ -20,38 +15,14 @@ import Link from '@mui/material/Link';
 import List from '@mui/material/List';
 import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Outlet, useLoaderData } from 'react-router-dom';
 
-import NewContactDialog from './NewContactDialog';
+import AppBar from './AppBar';
 import { User } from 'src/api/types';
 
 const defaultTheme = createTheme();
 const drawerWidth = 240;
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open'
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  })
-}));
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== 'open'
@@ -80,95 +51,18 @@ const Drawer = styled(MuiDrawer, {
 }));
 
 export default function ContactManager() {
-  const { t } = useTranslation();
-  const prefix = 'ContactManager.Toolbar.';
   const [drawerOpen, setDrawerOpen] = React.useState(true);
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
   };
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const menuOpen = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const [newContactDialogOpen, setNewContactDialogOpen] = React.useState(false);
-  const handleNewContactDialogOpen = () => {
-    setNewContactDialogOpen(true);
-    handleClose();
-  };
-  const handleNewContactDialogClose = () => {
-    setNewContactDialogOpen(false);
-  };
+
   const users = useLoaderData() as User[];
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="absolute" open={drawerOpen}>
-          <Toolbar
-            sx={{
-              pr: '24px' // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(drawerOpen && { display: 'none' })
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              {t(`${prefix}Title`)}
-            </Typography>
-            <IconButton
-              aria-label="more"
-              aria-controls={drawerOpen ? 'long-menu' : undefined}
-              aria-expanded={drawerOpen ? 'true' : undefined}
-              aria-haspopup="true"
-              onClick={handleClick}
-            >
-              <MoreVertIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={menuOpen}
-              onClose={handleClose}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-            >
-              <MenuItem onClick={handleNewContactDialogOpen}>
-                {t(`${prefix}Menu.NewContact`)}
-              </MenuItem>
-              <MenuItem disabled={true} onClick={handleClose}>
-                {t(`${prefix}Menu.ToggleTheme`)}
-              </MenuItem>
-              <MenuItem disabled={true} onClick={handleClose}>
-                {t(`${prefix}Menu.ToggleDir`)}
-              </MenuItem>
-            </Menu>
-          </Toolbar>
-        </AppBar>
+        <AppBar open={drawerOpen} toggleDrawer={toggleDrawer} />
         <Drawer variant="permanent" open={drawerOpen}>
           <Toolbar
             sx={{
@@ -218,10 +112,6 @@ export default function ContactManager() {
           </Container>
         </Box>
       </Box>
-      <NewContactDialog
-        open={newContactDialogOpen}
-        onClose={handleNewContactDialogClose}
-      />
     </ThemeProvider>
   );
 }
